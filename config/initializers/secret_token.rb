d4da800cb9d1d996eedf8aea78905d338af697fb
Weston Platter <westonplatter@gmail.com>
2013-04-01 19:34:38 -0500

    the beginning of awesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenCamp::Application.config.secret_token = '5dfb0bb6c8789b5ad61cfc746c8feef0ceecb5921b7d610f046529577afc97df16c55ba60ad51ed8ee12cb912c6e3de9ca13007321c465e150e182f86c88bb38'
