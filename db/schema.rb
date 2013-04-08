b2210e48521b5a138985aea8a60e964c99a6ef47
Weston Platter <westonplatter@gmail.com>
2013-04-08 17:46:35 -0500

    migrated database. ran this command, rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408045955) do
+ActiveRecord::Schema.define(:version => 20130408224450) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
