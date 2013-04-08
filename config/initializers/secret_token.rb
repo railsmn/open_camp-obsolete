290ca1fedc4c074ba3ead5e08bfe95b769e3cb1e
Weston Platter <westonplatter@gmail.com>
2013-04-08 17:40:12 -0500

    created rails app, ran this command, rails new open_camp -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenCamp::Application.config.secret_token = '90d4a32d26751f21c88d52fc14bb3c181e5cb2aa680a580fedde73a3df3d37ecc0286baf94895ad875d7a3cf23d957361d250fed6b5375e7d249f5941e9819cd'
