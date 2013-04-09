6f2400a10aaeade2c0a82f00d5112c475b6dfa71
Weston Platter <westonplatter@gmail.com>
2013-04-08 20:10:44 -0500

    don't include the open_camp rails app in the railsmn-dev-box

--- open_camp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OpenCamp::Application.config.secret_token = '5d6d7c69ceff57805310f20dd8f79244aa079fea32084a36bc4fc624a1d740e568a5d398e1166dcdce6783cd3accbfe9809240f8eb513a86726c7aaf353b4292'
