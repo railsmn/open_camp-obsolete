e4d55ca854959341076762e54023c8b49426a1dd
Weston Platter <westonplatter@gmail.com>
2013-05-05 23:35:18 -0500

    ran rake db:migrate and to add the notes table to the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408224450) do
+ActiveRecord::Schema.define(:version => 20130408224706) do
+
+  create_table "notes", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
