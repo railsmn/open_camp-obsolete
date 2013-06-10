c3b37a93e92f6ec717cc5d49f10a064974d2dc08
Derek Rockwell <derek.a.rockwell@gmail.com>
2013-06-10 10:58:25 -0500

    added simple validations to task object

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :description, :name
   belongs_to :user
+
+  validates :name, presence: true, length: {minimum: 5, maximum: 30}
+  validates :description, presence: true, length: {within: 2..160}
 end
