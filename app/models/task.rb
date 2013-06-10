41ca5a1cb71be1930912d3b2baeafdb5823abe8d
Derek Rockwell <derek.a.rockwell@gmail.com>
2013-06-10 12:48:18 -0500

    added task due date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,16 @@ class Task < ActiveRecord::Base
   attr_accessible :due_date, :description, :name
   belongs_to :user
 
+  validate :due_date_cannot_be_in_the_past
   validates :name, presence: true, length: {minimum: 5, maximum: 30}
   validates :description, presence: true, length: {within: 2..160}
+  
+
+  def due_date_cannot_be_in_the_past
+    unless due_date.blank?      
+      if due_date < Date.today
+        errors.add(:due_date,"cannot be in the past")
+      end
+    end
+  end
 end
