d5625b70961c5db25904aa346fb22d7562490258
Weston Platter <westonplatter@gmail.com>
2013-07-20 03:42:40 -0600

    nest Notes view under Project object

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,44 +1,52 @@
 class NotesController < ApplicationController
   def index
-    @notes = Note.all
+    @project = Project.find(params[:project_id])
+    @notes = @project.notes
   end
 
   def show
-    @note = Note.find(params[:id])
+    @project = Project.find(params[:project_id])
+    @note = Note.where(id: params[:id], project_id: @project.id).first
   end
 
   def new
+    @project = Project.find(params[:project_id])
     @note = Note.new
   end
 
   def edit
-    @note = Note.find(params[:id])
+    @project = Project.find(params[:project_id])
+    @note = Note.where(id: params[:id], project_id: @project.id).first
   end
 
   def create
-    @note = Note.new(params[:note])
+    @project = Project.find(params[:project_id])
+    @note = @project.notes.build(params[:note])
 
     if @note.save
-      redirect_to @note, notice: 'Note was successfully created.'
+      redirect_to project_note_path(@project, @note), notice: 'Note was successfully created.'
     else
       render action: "new"
     end
   end
 
-  def update    
-    @note = Note.find(params[:id])
+  def update   
+    @project = Project.find(params[:project_id]) 
+    @note = Note.where(id: params[:id], project_id: @project.id).first
 
     if @note.update_attributes(params[:note])
-      redirect_to @note, notice: 'Note was successfully updated.'
+      redirect_to project_note_url(@project, @note), notice: 'Note was successfully updated.'
     else
       render action: "edit"
     end
   end
 
   def destroy
-    @note = Note.find(params[:id])
+    @project = Project.find(params[:project_id])
+    @note = Note.where(id: params[:id], project_id: @project.id).first
+
     @note.destroy
 
-    redirect_to notes_url
+    redirect_to project_notes_url(@project)
   end
 end
