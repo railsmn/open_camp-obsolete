8b610e627658fd5db44742ff546571f0504e9ea7
Weston Platter <westonplatter@gmail.com>
2013-07-26 00:54:43 -0600

    added model relationships. tests pass.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,14 +2,15 @@ class Task < ActiveRecord::Base
   attr_accessible :due_date, :description, :name
   belongs_to :user
   belongs_to :project
+  belongs_to :task_list
 
   validate :due_date_cannot_be_in_the_past
   validates :name, presence: true, length: {minimum: 5, maximum: 30}
   validates :description, presence: true, length: {within: 2..160}
-  
+
 
   def due_date_cannot_be_in_the_past
-    unless due_date.blank?      
+    unless due_date.blank?
       if due_date < Date.today
         errors.add(:due_date,"cannot be in the past")
       end
--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,3 +1,4 @@
 class TaskList < ActiveRecord::Base
   attr_accessible :name
+  has_many :tasks
 end
